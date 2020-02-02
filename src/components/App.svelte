e0b7985398eaf75fa59795ccd70ead394c6e357c
Peter Meier <peter.meier.1986@gmail.com>
2020-02-02 14:13:35 +0100

    Create App.svelte

---
+++ src/components/App.svelte
@@ -0,0 +1,37 @@
+<script>
+    import Loadable from 'svelte-loadable'
+    import {fade} from 'svelte/transition';
+    export let name;
+    let loaded = false;
+    (() => {
+        setTimeout(() => {
+            loaded = !loaded
+        }, 2000)
+    })()
+</script>
+
+<style lang="scss">
+    .wrap {
+        display: flex;
+        min-width: 200px;
+        width: 30vw;
+        border: 1px solid black;
+        padding: 1rem;
+    }
+    h1 {
+        span.asyncText {
+            color: green;
+        }
+    }
+</style>
+
+<h1>
+    <span>
+        Hello..
+    </span>
+    {#if loaded}
+        <span class="asyncText" transition:fade="{{duration: 200}}">
+            <Loadable txt="{name}" loader={() => import('./Async.svelte')}/>
+        </span>
+    {/if}
+</h1>
